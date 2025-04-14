639b764560bac38cb3f7ea51ff73f10782041fe3
Luiz Prado <105995236+LuizEduardoPrado@users.noreply.github.com>
2025-04-13 23:21:20 -0300

    Update Meuapp.py

--- Meuapp.py
+++ Meuapp.py
@@ -20,11 +20,6 @@ components.html(f'''
 # URL direta para o PDF (link raw do GitHub)
 pdf_url = "https://raw.githubusercontent.com/LuizEduardoPrado/LuizPrado/main/LUIZ%20PRADO%20CV.pdf"
 
-# Exibe o PDF com iframe
-components.html(f'''
-    <iframe src="{pdf_url}" width="700" height="1000" type="application/pdf"></iframe>
-''', height=1000)
-
 # Botão de download / abrir em nova aba
 st.markdown(f"[📄 Clique aqui para abrir o PDF em nova aba]({pdf_url})")
 
